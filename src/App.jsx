a764af06160f234a375e1bbfe166b0721b26732c
Kiefer Villarreiz <kvillarr@nyit.edu>
2026-04-29 19:46:30 -0400

    Core Concepts implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,55 @@
+
+import componentsImg from './assets/components.png';
+import {CORE_CONCEPTS} from './data';
+import Header from './components/Header';
+import CoreConcept from './components/CoreConcept'
+//function UserData(){
+ // const user = {first:"Kiefer", last:"Villarreiz", age:"22"};
+  //return(
+ // <div>
+ //   <h2>{user.last}, {user.first}</h2>
+ //   <h3>Age: {user.age}</h3>
+  //</div>
+ // )
+//}
+
+
+
+
+
+
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+     <Header />
       <main>
-        <h2>Time to get started!</h2>
+       <section id = "core-concepts">
+         <h2>Time to get started!</h2>
+        <ul>
+          <CoreConcept 
+          {
+            ...CORE_CONCEPTS[0]
+          }
+          />
+           <CoreConcept  
+           {
+            ...CORE_CONCEPTS[1]
+          }/>
+            <CoreConcept 
+             {
+            ...CORE_CONCEPTS[2]
+          }/>
+             <CoreConcept 
+                {
+            ...CORE_CONCEPTS[3]
+             //CORE_CONCEPT.map((concept))=>CoreConcept {...concept}/>)
+          }/>
+         
+        </ul>
+       </section>
       </main>
+
     </div>
   );
 }
